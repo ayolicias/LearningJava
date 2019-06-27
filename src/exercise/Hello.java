d6ba32cd349cc4907cf2d1a604942688929c9f79
ayolicias <ziyandandlamafa47@gmail.com>
2019-06-27 17:14:47 +0200

    initial commit

---
+++ src/exercise/Hello.java
@@ -0,0 +1,12 @@
+package exercise;
+
+public class Hello {
+
+    public static void main (String[] args){
+        // Entry point to the program
+        // Write code here
+
+        System.out.println("Hello Wold");
+        System.out.println("This is my message");
+    }
+}
---
+++ src/exercise/ShoppingCart.java
@@ -0,0 +1,36 @@
+package exercise;
+
+public class ShoppingCart {
+
+    public static void main (String args[]){
+        // Declare and initials Strings variables
+
+        String custName = "Ziya", itemDesc ="Shirt";
+
+
+        // Assign the message variable.
+        String message = custName + " " + itemDesc + " ";
+
+        // Output Results
+        System.out.println("Welcome to the shopping Cart!");
+        System.out.println("message");
+        System.out.println(custName + " wants to purchase a " + itemDesc + "!");
+
+
+        int quantity = 0;
+        boolean outOfStock = true;
+
+        if (quantity > 1){
+
+        }
+        else if (outOfStock = false){
+
+        }
+        else {
+            System.out.println("message");
+            System.out.println("");
+        }
+
+    }
+
+}
